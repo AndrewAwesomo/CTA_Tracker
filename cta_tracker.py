2f4c52e06a21342ed63d8a4413462f4dfede6160
Drew <andrew.hosford@gmail.com>
2018-02-06 19:53:24 -0600

    added clock and some formatting

--- cta_tracker.py
+++ cta_tracker.py
@@ -13,14 +13,34 @@ TRAIN_STATION = '41410'  # Chicago (Blue)
 BUS_STATION = '563,615,5500,5525'
 UPDATE_TIME = 15  # set number of seconds between updates
 
+class Clock:
+    def __init__(self, master):
+        self.master = master
+        self.update_frame()
+
+    def update_frame(self):
+        try:
+            self.frame.destroy() # get rid of any old container frames so the new labels don't stack on each other
+        except:
+            pass
+        self.frame = Frame(self.master)  # create new parent frame to jam each train direction frame into
+        self.frame.pack()
+        t = datetime.now().time()
+        time = t.strftime('%I:%M:%S.%f%p')[:-9] + ' ' + t.strftime('%I:%M:%S.%f%p')[-2:]
+        self.clockLabel = Label(self.frame, text=time)
+        self.clockLabel.config(font=('Courier', 50))
+        self.clockLabel.grid(row=0, sticky=N+W+S+E)
+        self.master.after(1000, self.update_frame)
+
 
 class Train:
     def __init__(self, master, TRAIN_API_KEY, TRAIN_STATION, UPDATE_TIME):
         self.TRAIN_API_KEY = TRAIN_API_KEY
         self.TRAIN_STATION = TRAIN_STATION
         self.UPDATE_TIME = UPDATE_TIME
-        self.heading = Label(master, text='Train Tracker')
-        self.heading.pack()
+        self.heading = Label(master, text='Train Tracker', bg='green', fg='white')
+        self.heading.config(font=('Courier', 20))
+        self.heading.pack(fill=X)
         self.master = master
         self.update_frame()
 
@@ -43,8 +63,9 @@ class Bus:
         self.BUS_API_KEY = BUS_API_KEY
         self.BUS_STATION = BUS_STATION
         self.UPDATE_TIME = UPDATE_TIME
-        self.heading = Label(master, text='Bus Tracker')
-        self.heading.pack()
+        self.heading = Label(master, text='Bus Tracker', bg='green', fg='white')
+        self.heading.config(font=('Courier', 20))
+        self.heading.pack(fill=X)
         self.master = master
         self.update_frame()
 
@@ -124,11 +145,16 @@ def formatEta(eta):
 
 root = Tk()
 root.winfo_toplevel().title('CTA Tracker: Updates Every {} seconds'.format(UPDATE_TIME))
+
 trainFrame = Frame(root, padx=5, bd=2, relief=GROOVE)
 trainFrame.grid(row=0, column=0, sticky=NW)
 Train(trainFrame, TRAIN_API_KEY, TRAIN_STATION, UPDATE_TIME)
 
+clockFrame = Frame(root, padx=5, bd=2, relief=GROOVE)
+clockFrame.grid(row=0, column=1, sticky=W)
+Clock(clockFrame)
+
 busFrame = Frame(root, padx=5, bd=2, relief=GROOVE)
-busFrame.grid(row=2, column=0, sticky=NW)
+busFrame.grid(row=1, column=0, sticky=NW, columnspan=2)
 Bus(busFrame, BUS_API_KEY, BUS_STATION, UPDATE_TIME)
 root.mainloop()
